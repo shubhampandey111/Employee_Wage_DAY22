90e86b20a7572b854a3fa9e721c231e93bc8add8
shubhampandey111 <shubhampandey051197@gmail.com>
2020-11-13 22:57:56 +0530

    UC4- Using Switch Case

--- Employeewage.java
+++ Employeewage.java
@@ -1,23 +1,26 @@
+import java.util.Random;
 public class Employeewage {
-    public static void main(String arg[]){
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+	public static final int empRatePerHr=20;
+    public static final int is_Part_Time=2;
+    public static final int is_Full_Time=1;
+    public static void main(String args[]){
+        System.out.println("WELCOME to EMPLOYEE WAGE Computation");
         int empRatePerHr=20;
-        int is_Part_Time=2;
-        int is_Full_Time=1;
         int empHrs=0;
         int empWage=0;
-        double employee = (int)(( Math.random() * 10 ) % 3);
-        if ( employee == is_Part_Time ) {
-            System.out.println("Part Time  ");
-            empHrs = 4;
-        }
-        else if (employee == is_Full_Time ) {
-            System.out.println("Full Time ");
-            empHrs = 8;
-        }
-        else {
-            System.out.println("Employee is Absent ");
-            empHrs = 0;
+        int employee=(int)(( Math.random()*10)%3);
+        switch(employee){
+            case is_Part_Time:
+                System.out.println("Part Time  ");
+                empHrs = 4;
+                break;
+            case is_Full_Time:
+                System.out.println("Full Time ");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent ");
+                empHrs = 0;
         }
             empWage=(empHrs*empRatePerHr);
             System.out.println("Employee Wage is "+empWage);
